e23df2526e613246ef797d3f8ed13eca7981a257
Dagzounet <paul.gamot@gmail.com>
2023-09-18 01:50:38 +0200

    added some comments to card and fetchdata component
    added some comments to card and fetchdata component

--- project8kasa/src/components/Card/Card.js
+++ project8kasa/src/components/Card/Card.js
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import FetchData from "../FetchData/FetchData";
 
 function Card() {
-  const [logements, setLogements] = useState([]);
+  const [logements, setLogements] = useState([]); // useState pour pouvoir stocker les données des logements
 
   const handleDataFetched = (data) => {
-    setLogements(data);
+    // Appel de la fonction pour traiter les données récupérées
+    setLogements(data); // Mise à jour du useState avec les données récupérées
   };
 
   return (
     <div className="gallery">
       <FetchData onDataFetched={handleDataFetched} />
+      {/* Appel du composant pour le fetch des données 
+          puis boucle map avec les données des logements pour les afficher*/}
       {logements.map((logement, index) => (
         <figure key={index}>
           <img src={logement.cover} alt={logement.title} />
--- project8kasa/src/components/FetchData/FetchData.js
+++ project8kasa/src/components/FetchData/FetchData.js
@@ -3,19 +3,20 @@ import { useEffect } from "react";
 function FetchData({ onDataFetched }) {
   useEffect(() => {
     const fetchData = async () => {
+      // Fonction asynchrone pour récupérer les données
       try {
-        const response = await fetch("/logements.json");
-        const data = await response.json();
-        onDataFetched(data);
+        const response = await fetch("/logements.json"); // requête HTTP pour obtenir les données
+        const data = await response.json(); // Réponse en format JSON
+        onDataFetched(data); // Appel de la fonction onDataFetched avec les données récupérées
       } catch (error) {
         console.error("Erreur lors de la récupération des données :", error);
       }
     };
 
     fetchData();
-  }, [onDataFetched]);
+  }, [onDataFetched]); // Appel de la fonction fetchData lorsque le composant est monté
 
-  return null;
+  return null; // pour indiquer qu'il n'y a pas de rendu immédiat (conventionnel)
 }
 
 export default FetchData;
