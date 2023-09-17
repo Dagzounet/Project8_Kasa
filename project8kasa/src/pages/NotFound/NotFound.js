0bae00b0b73ddfd97f6a326eb47258d2911e8a6a
Dagzounet <paul.gamot@gmail.com>
2023-09-17 19:44:53 +0200

    made necessary scss adjustment to 404 notfound webpage
    made necessary scss adjustment to 404 notfound webpage

--- project8kasa/src/pages/NotFound/NotFound.js
+++ project8kasa/src/pages/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotFound() {
   return (
-    <div>
+    <div className="notfound">
       <h2>404</h2>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <a href="/">Retourner sur la page d'accueil.</a>
