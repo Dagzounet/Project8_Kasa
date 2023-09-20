d57bdc4bebd230223d8378578a853e205adf2d0c
Dagzounet <paul.gamot@gmail.com>
2023-09-21 00:21:40 +0200

    fetch creation to be able to show the right information for the right id for each housing page
    fetch creation to be able to show the right information for the right id for each housing page

--- project8kasa/src/pages/Housing/Housing.js
+++ project8kasa/src/pages/Housing/Housing.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Housing() {
+  const { id } = useParams();
+  const [logement, setLogement] = useState(null);
+
+  useEffect(() => {
+    const fetchLogement = async () => {
+      try {
+        const response = await fetch("/logements.json");
+        const data = await response.json();
+        const logementData = data.find((item) => item.id === id);
+
+        if (logementData) {
+          setLogement(logementData);
+        } else {
+          console.error("Logement introuvable");
+        }
+      } catch (error) {
+        console.error("Erreur lors de la récupération du logement :", error);
+      }
+    };
+
+    fetchLogement();
+  }, [id]);
+
+  if (!logement) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h2>Cozy loft on the Canal Saint-Martin</h2>
-      <p>Paris, Île-de-France</p>
+      <h2>{logement.title}</h2>
+      <p>{logement.location}</p>
+      <p>{logement.tags}</p>
+      <p>
+        <img src={logement.host.picture} alt="photo du propriétaire" />
+        {logement.host.name}
+        {logement.rating}
+      </p>
+      <div>
+        <h2>Description</h2>
+        <p>{logement.description}</p>
+      </div>
+      <div>
+        <h2>Equipements</h2>
+        <p>{logement.description}</p>
+      </div>
     </div>
   );
 }
